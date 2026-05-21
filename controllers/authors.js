93d51eab570998a67886f167b12ff3e7d11fb181
aungkokodev <aungkokoepic@gmail.com>
2026-05-21 13:31:53 +0630

    Exercise 16

---
+++ controllers/authors.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+const { Blog } = require('../models')
+const sequelize = require('sequelize')
+
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('id')), 'articles'],
+      [sequelize.fn('COALESCE', sequelize.fn('SUM', sequelize.col('likes')), 0), 'likes'],
+    ],
+    group: 'author',
+  })
+
+  res.json(authors)
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { connectToDatabase } = require('./util/db')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const authorsRouter = require('./controllers/authors')
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/authors', authorsRouter)
 
 const errorHandler = (error, req, res, next) => {
   console.log('----------Error----------')
